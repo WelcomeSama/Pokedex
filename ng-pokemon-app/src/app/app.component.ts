edb46a1e89dd88fdb02c87132778f02d32f11972
Brandon Vital <bran.vital@gmail.com>
2022-05-12 15:43:33 +0200

    first commit

---
+++ ng-pokemon-app/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Welcome to {{ pokemons[1] }}!</h1>`
+})
+export class AppComponent {
+  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+}
