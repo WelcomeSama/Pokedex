3c1c81030bbb2576b9c2aabf64c228d504cdaa46
Brandon Vital <bran.vital@gmail.com>
2022-05-18 18:14:58 +0200

    auth

--- ng-pokemon-app/src/app/app.module.ts
+++ ng-pokemon-app/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { PokemonModule } from './pokemon/pokemon.module';
 import { PokemonService } from './pokemon/pokemon.service';
 import { InMemoryDataService } from './in-memory-data.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
